f9bb6a4b8f4b3ee3d077e13efad3b17123f6f7be
Sadek Drobi <github@sadekdrobi.com>
2010-02-14 12:10:07 +0100

    experimenting with a slight different design. For now it doesnt pass tests

--- SomeEarlyTThinking/SomeTThinking/Module1.fs
+++ SomeEarlyTThinking/SomeTThinking/Module1.fs
@@ -25,9 +25,9 @@ and Dimensions = EntityName * Year * Month
 and RelativeEntityTypeLevel = int
 and ContextTrans = MatrixContext -> MatrixContext
 
-let buildContextKey rawkey ctx = match ctx with 
-                                   CellContext((entityname, year, month), _) -> String.Format("{0}.{1}.{2}.{3}", entityname, rawkey, year, month)
-                                   | GlobalContext -> rawkey
+let buildContextKey rawkey  =
+             function CellContext((entityname, year, month), _) -> String.Format("{0}.{1}.{2}.{3}", entityname, rawkey, year, month)
+                    | GlobalContext -> rawkey
 
 let applyContextTrans (contextTrans:ContextTrans) ctx = contextTrans ctx
 
@@ -70,6 +70,10 @@ let ctxIntoEnv ctx env =    let tenv = if (Map.containsKey("YEAR") env) then (Ma
                             match ctx with
                             CellContext((_, y, m), _) -> Map.add "MONTH" (DoubleVal(m)) (Map.add "YEAR" (DoubleVal(y)) tenv')
                             |_-> env
+
+
+
+
 
 
 type CalcMap = class
@@ -229,26 +233,16 @@ let max (a:double) (b:double) = Math.Max(a,b)
 let min (a:double) (b:double) = Math.Min(a,b)
 
 
+type CalcStore=Dictionary<string,Exp>
+type QualifiedName=  MatrixContext * String
 
-let rec eval env (cmap:CalcMap) (ctx:MatrixContext) = 
-        let gotogetExp name = cmap.findByKey(name)
-        let updateCell (cmap:CalcMap) name c = cmap.update name c
+
+let rec eval env  (ctx:MatrixContext) = 
+        let gotogetExp name = storeCache name
         let gotogetValue name trans (env:Env) = let newCtx = trans ctx
-                                                let ctxQuery = buildContextKey name newCtx
-                                                //attention "Option.get" here raises exception with no functional meaning
-                                                let cell = let firstTry=gotogetExp ctxQuery in 
-                                                             firstTry |> getOrElse <| lazy((gotogetExp name).Value) in 
-                                                    match(cell) with
-                                                            ValuedCell(v, _) -> v
-                                                            | NonValuedCell(e) -> 
-                                                                    let newEnv= ctxIntoEnv newCtx env
-                                                                    let v = eval newEnv cmap newCtx e
-                                                                    updateCell cmap ctxQuery (ValuedCell(v, e))
-                                                                    v
-                                                            | FixedCell(v) -> v
-                                                            | EmptyCell -> DoubleVal(0.)
+                                                in gotogetExp (name, newCtx) env
 
-        let app op a b = match((eval (ctxIntoEnv ctx env) cmap ctx a,eval (ctxIntoEnv ctx env) cmap ctx b)) with
+        let app op a b = match((eval (ctxIntoEnv ctx env)  ctx a,eval (ctxIntoEnv ctx env)  ctx b)) with
                                                     (DoubleVal(d1),DoubleVal(d2))-> DoubleVal (op d1 d2)
                                                     |_ -> raise(Exception())
         in
@@ -260,7 +254,6 @@ let rec eval env (cmap:CalcMap) (ctx:MatrixContext) =
                                             let ((entityname,year,month),entitygraph)  = match ctx with
                                                                                          CellContext((n, y, m), g) -> ((n, y, m), g)
                                                                                          |_->raise(Exception())
-                                            in
                                             let ownership = match getEntityRelations entitygraph entityname with Owns(list) -> list
                                             in
                                             match fold with
@@ -270,7 +263,7 @@ let rec eval env (cmap:CalcMap) (ctx:MatrixContext) =
                                                     (owned,r)::t -> 
                                                         let newCtx = CellContext((owned, year, month), entitygraph)
                                                         let newEnv= ctxIntoEnv newCtx env
-                                                        match (eval newEnv cmap newCtx e) with
+                                                        match (eval newEnv newCtx e) with
                                                         DoubleVal(d1) -> foldsum t (acc + (d1*r))
                                                         |_->raise(Exception())
                                                     |[]-> DoubleVal(acc)
@@ -281,14 +274,17 @@ let rec eval env (cmap:CalcMap) (ctx:MatrixContext) =
                  | Max(e1, e2) -> app max e1 e2
                  | Min(e1, e2) -> app min e1 e2
                  | Fun(name,e)-> FunVal(env,name,e)
-                 | App (ef,e1) ->  match(eval (ctxIntoEnv ctx env) cmap ctx ef) with
+                 | App (ef,e1) ->  match(eval (ctxIntoEnv ctx env) ctx ef) with
                                      FunVal(env,name,e)->
-                                        let newEnv=Map.add name (eval (ctxIntoEnv ctx env) cmap ctx e1) env
-                                        in eval newEnv cmap ctx e
+                                        let newEnv=Map.add name (eval (ctxIntoEnv ctx env) ctx e1) env
+                                        in eval newEnv ctx e
                                      |_ -> raise (Exception())
 
-
-
+and calcStore= new Dictionary<string,Exp>()
+and qualifiedKey (context,key)= buildContextKey key context
+and cache= System.Collections.Concurrent.ConcurrentDictionary<string,Value>() 
+and storeCache (key,context) env=let qualifiedKey= buildContextKey key context in
+                                        cache.GetOrAdd(qualifiedKey, fun k-> eval env context calcStore.[qualifiedKey]) 
 
 let (<+>) a b = Plus (a,b)
 let ($) = appN
@@ -297,12 +293,12 @@ let nulContextTrans (c:MatrixContext) = c
 let previousYearTrans (c:MatrixContext) = match c with
                                           CellContext((entityname, year, month), g) -> CellContext((entityname, year-1., month), g)
                                           | _ -> c
-
+let globalYearTrans _= GlobalContext
 let var a = Binding(a, nulContextTrans)
 
 // Samples
  
-let calcMap = CalcMap()
+//let calcMap = CalcMap()
 let env0:Env= Map.empty
 
 
@@ -315,67 +311,68 @@ entityGraph.Add("OtherHolding", Owns [("CompanyZ", 0.10); ("CompanyW", 0.20)])
 
 let cellCtx entityName (year:int) (month:int) = CellContext((entityName, Convert.ToDouble(year), Convert.ToDouble(month)), entityGraph)
 
-calcMap.registerVal (cellCtx "entity1" 2009 1) "rent" (DoubleVal(100.))
-calcMap.registerVal (cellCtx "entity1" 2013 1) "rent" (DoubleVal(200.))
-calcMap.registerExp (cellCtx "entity1" 2010 1) "charges" (Const(-10.)) []
-calcMap.registerExp (cellCtx "entity1" 2013 1) "charges" (Const(-50.)) []
-calcMap.registerExp (cellCtx "entity1" 2010 1) "works" (Const(-20.)) []
-calcMap.registerVal (cellCtx "entity1" 2014 1) "rent" (DoubleVal(200.))
-calcMap.registerVal (cellCtx "entity1" 2014 1) "charges" (DoubleVal(-50.))
-calcMap.registerVal (cellCtx "entity1" 2015 1) "rent" (DoubleVal(200.))
-calcMap.registerVal (cellCtx "entity1" 2015 1) "charges" (DoubleVal(-50.))
-calcMap.registerVal (cellCtx "entity1" 2016 1) "rent" (DoubleVal(200.))
-calcMap.registerVal (cellCtx "entity1" 2016 1) "charges" (DoubleVal(-50.))
+calcStore.Add (qualifiedKey (cellCtx "entity1" 2009 1, "rent") ,(Const(100.)))
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2013 1, "rent"), Const 200.)
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2010 1, "charges"), Const -10.) 
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2013 1, "charges"),Const -50.) 
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2010 1, "works"), Const -20.)
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2014 1, "rent"), Const 200.)
+calcStore.Add (qualifiedKey (cellCtx "entity1" 2014 1, "charges"),Const -50.)
+calcStore.Add (qualifiedKey (cellCtx "entity1" 2015 1, "rent"),Const 200.)
+calcStore.Add (qualifiedKey (cellCtx "entity1" 2015 1, "charges"),Const -50.)
+calcStore.Add (qualifiedKey (cellCtx "entity1" 2016 1, "rent"), Const 200.)
+calcStore.Add (qualifiedKey (cellCtx "entity1" 2016 1, "charges" ),Const -50.)
 
 
-calcMap.registerExp GlobalContext "natureX" (funN ["a"; "b"] (var "a" <+> var "b") $ [var "charges"; var "rent"]) [var "charges"; var "rent"]
-calcMap.registerExp (cellCtx "entity1" 2010 1) "natureX" (funN ["a"; "b"; "c"] (var "a" <+> var "b" <+> var "c") $ [var "charges"; Binding("rent", previousYearTrans); var "works"]) [var "charges"; Binding("rent", previousYearTrans); var "works"]
-calcMap.registerExp (cellCtx "entity1" 2011 1) "natureX" (Binding("natureX", previousYearTrans)) [Binding("natureX", previousYearTrans)]
-calcMap.registerExp (cellCtx "entity1" 2012 1) "natureX" (Binding("natureX", previousYearTrans)) [Binding("natureX", previousYearTrans)]
+calcStore.Add (qualifiedKey (GlobalContext, "natureX"), (funN ["a"; "b"] (var "a" <+> var "b") $ [var "charges"; var "rent"])) 
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2010 1, "natureX"), funN ["a"; "b"; "c"] (var "a" <+> var "b" <+> var "c") $ [var "charges"; Binding("rent", previousYearTrans); var "works"])
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2011 1, "natureX"), (Binding("natureX", previousYearTrans)))
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2012 1, "natureX"), (Binding("natureX", previousYearTrans)))
 
-calcMap.registerExp (cellCtx "entity1" 2010 1) "natureYY" (var "natureX") [var "natureX"]
-calcMap.registerExp (cellCtx "entity1" 2011 1) "natureYY" (Binding("natureYY", previousYearTrans)) [Binding("natureYY", previousYearTrans)]
-calcMap.registerExp (cellCtx "entity1" 2012 1) "natureYY" (Binding("natureYY", previousYearTrans)) [Binding("natureYY", previousYearTrans)]
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2010 1, "natureYY"), (Binding ("natureX",previousYearTrans)) )
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2011 1, "natureYY"), (Binding("natureYY", previousYearTrans)))
+calcStore.Add (qualifiedKey(cellCtx "entity1" 2012 1, "natureYY"), (Binding("natureYY", previousYearTrans)) )
 
 
 // simple calculation
-let natureX2010 = eval env0 calcMap (cellCtx "entity1" 2010 1) (var "natureX")
+let natureX2010 = eval env0 (cellCtx "entity1" 2010 1) (var "natureX")
 // should not be reevaluated twice
-let natureX2010_ = eval env0 calcMap (cellCtx "entity1" 2010 1) (var "natureX")
+let natureX2010_ = eval env0 (cellCtx "entity1" 2010 1) (var "natureX")
 
 // calculation based on anther cell context (allows time and dimensions browsing)
-let natureX2012 = eval env0 calcMap (cellCtx "entity1" 2012 1) (var "natureX")
+let natureX2012 = eval env0 (cellCtx "entity1" 2012 1) (var "natureX")
 // again, should not be reevaluated twice
-let natureX2012_ = eval env0 calcMap (cellCtx "entity1" 2012 1) (var "natureX")
+let natureX2012_ = eval env0 (cellCtx "entity1" 2012 1) (var "natureX")
 
 // a default formula must be used if no formula is defined for the nature & cell context : here natureX from GlobalContext 
-let natureX2013 = eval env0 calcMap (cellCtx "entity1" 2013 1) (var "natureX")
-let natureX2014 = eval env0 calcMap (cellCtx "entity1" 2014 1) (var "natureX")
-let natureX2015 = eval env0 calcMap (cellCtx "entity1" 2015 1) (var "natureX")
-let natureX2016 = eval env0 calcMap (cellCtx "entity1" 2016 1) (var "natureX")
+let natureX2013_ = eval env0 (cellCtx "entity1" 2013 1) (var ("natureX"))
+let natureX2013 = eval env0 (cellCtx "entity1" 2013 1) (var "natureX")
+let natureX2014 = eval env0 (cellCtx "entity1" 2014 1) (var "natureX")
+let natureX2015 = eval env0 (cellCtx "entity1" 2015 1) (var "natureX")
+let natureX2016 = eval env0 (cellCtx "entity1" 2016 1) (var "natureX")
 
 // can navigate among natures and time
-let natureYY2012 = eval env0 calcMap (cellCtx "entity1" 2012 1) (var "natureYY")
+let natureYY2012 = eval env0 (cellCtx "entity1" 2012 1) (var "natureYY")
 
 // Can change the formula for a given cell. Dependencies must be updated and change must be propagated so that dependents are recalculated
-calcMap.changeTo (cellCtx "entity1" 2010 1) "natureX" (funN ["a"; "b"] (var "a" <+> var "b") $ [var "charges"; Binding("rent", previousYearTrans)]) [var "charges"; Binding("rent", previousYearTrans)]
-let natureYY2012_ = eval env0 calcMap (cellCtx "entity1" 2012 1) (var "natureYY")
+calcStore.[qualifiedKey (cellCtx "entity1" 2010 1, "natureX")] <- (funN ["a"; "b"] (var "a" <+> var "b") $ [var "charges"; Binding("rent", previousYearTrans)])
+let natureYY2012_ = eval env0 (cellCtx "entity1" 2012 1) (var "natureYY")
 
 
 // holding rent is calculated based on children rents, using an aggregation function (Sum)
-calcMap.registerVal (cellCtx "CompanyA" 2009 1) "rent" (DoubleVal(100.)) 
-calcMap.registerExp (cellCtx "Holding" 2010 1) "rent" (Children(Sum, Binding("rent", previousYearTrans))) [Children(Sum, Binding("rent", previousYearTrans))]
-let holdingrent = eval env0 calcMap (cellCtx "Holding" 2010 1) (var "rent")
+calcStore.Add (qualifiedKey(cellCtx "CompanyA" 2009 1, "rent"), Const 100.) 
+calcStore.Add (qualifiedKey(cellCtx "Holding" 2010 1, "rent"), (Children(Sum, Binding("rent", previousYearTrans)))) 
+let holdingrent = eval env0 (cellCtx "Holding" 2010 1) (var "rent")
 
 
 // holding rent is calculated taking in account ownership ratio of children
-calcMap.registerVal (cellCtx "CompanyW" 2009 1) "rent" (DoubleVal(100.)) 
-calcMap.registerVal (cellCtx "CompanyZ" 2009 1) "rent" (DoubleVal(900.)) 
-calcMap.registerExp (cellCtx "OtherHolding" 2010 1) "rent" (Children(Sum, Binding("rent", previousYearTrans))) [Children(Sum, Binding("rent", previousYearTrans))]
-let otherholdingrent = eval env0 calcMap (cellCtx "OtherHolding" 2010 1) (var "rent")
+calcStore.Add (qualifiedKey(cellCtx "CompanyW" 2009 1, "rent"), Const 100.) 
+calcStore.Add (qualifiedKey(cellCtx "CompanyZ" 2009 1, "rent"), Const 900.) 
+calcStore.Add (qualifiedKey(cellCtx "OtherHolding" 2010 1, "rent"), (Children(Sum, Binding("rent", previousYearTrans))))
+let otherholdingrent = eval env0 (cellCtx "OtherHolding" 2010 1) (var "rent")
 
 // allow using YEAR in the formula as a reference to the contextual date
-let testYEAR = eval env0 calcMap (cellCtx "Holding" 2010 1) (funN ["a"; "b"] (var "a" <+> var "b") $ [var "YEAR"; Const(10.)])
+let testYEAR = eval env0 (cellCtx "Holding" 2010 1) (funN ["a"; "b"] (var "a" <+> var "b") $ [var "YEAR"; Const(10.)])
 
 
 
