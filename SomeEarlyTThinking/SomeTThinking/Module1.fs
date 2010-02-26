c2116a50358df27010fbe71487bcf748f8c92cbd
Sadek Drobi <github@sadekdrobi.com>
2010-02-26 15:21:19 +0100

    deleted things that will stay on developement branches (like dependecies and agents) to have a clean version on master

--- SomeEarlyTThinking/SomeTThinking/Module1.fs
+++ SomeEarlyTThinking/SomeTThinking/Module1.fs
@@ -3,7 +3,6 @@ open System
 open System.Collections.Generic
 open Exp
 open UsefulStuff
-open AgentSystem.LAgent
 
 let  minT:int ref =ref 0
 let  minIOT:int ref =ref 0
@@ -30,23 +29,8 @@ and Value= DoubleVal of double
 
 type CalcStore=Dictionary<string,Exp>
 type QualifiedName=  ExecutionContext * String
-open Microsoft.FSharp.Control
-let rec collectExDependencies exp (ctxt:ExecutionContext)= 
-        let rec collect dependencies= 
-                   function |_ when not (ctxt.IsConsistent) -> dependencies
-                            |Const _|ConstB _| Binding _ |Context _->  dependencies
-                            |Ref(name,trans)->  (name,trans ctxt)::dependencies
-                            |BinaryExp(_,e1,e2) |App (e1,e2)-> collect (collect dependencies e1) e2
-                            |Fun(_,e) ->  collect dependencies e
-                            |Children(fold, e) -> 
-                                            let dims, entitydepFun  = match ctxt with CellContext(d, entitydepFun) -> d, entitydepFun
-                                            let (_,Owns(ownership)) =ctxt.EntityDependencies 
-                                            in List.fold (fun ds (owned,_)-> let newCtx = CellContext({dims with entity=owned}, entitydepFun)
-                                                                             in ds @ (collectExDependencies e newCtx) ) dependencies ownership  
-                            |If (condition,_,_) -> collect dependencies condition
-        collect [] exp
-                     
-let mutable isOk= 0
+                    
+let mutable inParallel=true
 
 let rec eval (env :Env) = 
   let app op a b = match(eval env a,eval env b) with
@@ -60,11 +44,10 @@ let rec eval (env :Env) =
                 match dimension,env.context with Year ,CellContext(ds,_) ->  DoubleVal (Convert.ToDouble( referenceDate.AddMonths(ds.date).Year)) 
                                                 |Month ,CellContext(ds,_) ->  DoubleVal (Convert.ToDouble( referenceDate.AddMonths(ds.date).Month)) 
                                                                                                        
-             |Ref(name,trans)-> evaluatedCells <- evaluatedCells+1
-                                (gotogetValue name trans env)  
+             |Ref(name,trans)-> (gotogetValue name trans env)  
              |Children(fold, e) ->  let ds, entitydepFun  = match env.context with CellContext(d, f) -> d, f
                                     let (_,Owns(ownership)) =env.context.EntityDependencies 
-                                    let childrenEvaluated= let map= if(true)then let _= isOk<-isOk+1 in Parallels.map else Seq.map
+                                    let childrenEvaluated= let map= if(inParallel)then Parallels.map else Seq.map
                                                            in  map  (fun(owned,r) -> let  newCtx = CellContext({ds with entity=owned}, entitydepFun)
                                                                                      let newEnv= {env with context= newCtx}
                                                                                      in((eval newEnv e),r) ) ownership                                      
@@ -92,33 +75,20 @@ let rec eval (env :Env) =
 and gotogetValue name trans (env:Env) = let newEnv = {env with  context=trans env.context }
                                         if(not(newEnv.context.IsConsistent)) then DoubleVal 0.
                                         else let task= (storeCache (name, newEnv.context) newEnv.bindigs )
-                                             in (snd <| task.Force())
+                                             in ( task.Force())
 
 and calcStore= new Dictionary<string,Exp>()
 and getCalcFromStore qualifiedKey= if calcStore.ContainsKey qualifiedKey then Some calcStore.[qualifiedKey] else None
 and qualifiedKey (context,key)= buildContextKey key context
 
-and cache= System.Collections.Concurrent. ConcurrentDictionary<string,Lazy<Set<string>*Value>>(100,10000) 
+and cache= System.Collections.Concurrent. ConcurrentDictionary<string,Lazy<Value>>(100,10000) 
 
-and storeCache (key,context) env :Lazy<Set<string>*Value>=     
+and storeCache (key,context) env :Lazy<Value>=     
     let qualifiedKey= match context with CellContext(ds,_)-> buildContextKey key (Cell(ds))
     let valueFactory= fun k->lazy(let entityT= match context with CellContext(ds,_)->ds.entity.etype 
                                   let partialKey= buildContextKey key (Partial {entityType=Some entityT})
                                   let exp= getCalcFromStore qualifiedKey |> getOrElse <| lazy((getCalcFromStore partialKey) |> getOrElse <| lazy( defaultArg (getCalcFromStore key) (Const 0.)))
-    
-                                  //let dependencies=collectExDependencies exp context
                                   let newEnv={bindigs=env ;context=context}
-                                  //let _=  worker <-- ( fun()-> List.iter (reverse storeCache env >> ignore)    dependencies) 
-                                  //I need to store dependencies rather with EXP rather than with values
-                                  in (Set.empty(*Set.ofList(List.map (fun (key,cxt)->buildContextKey key cxt) dependencies)*),eval newEnv exp))
+                                  in eval newEnv exp)
     in cache.GetOrAdd(qualifiedKey,valueFactory)
-                                                              
-//and worker = spawnParallelWorker (fun f -> //printfn "doing some Work"
- //                                          f()) 10
-(* open AgentSystem.LAgent
-let agent =
-   Agent.Start(fun inbox ->
-     async { while true do
-               let! msg = inbox.Receive()
-               printfn "got message '%s'" msg } ) *)
 
