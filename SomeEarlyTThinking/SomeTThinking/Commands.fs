418c2d19e37bc73a77e7d0d9b0832b13086038f8
Sadek Drobi <github@sadekdrobi.com>
2010-03-07 18:26:52 +0100

    forgot the commands file

---
+++ SomeEarlyTThinking/SomeTThinking/Commands.fs
@@ -0,0 +1,7 @@
+﻿module Commands
+open Exp
+type Commands= Unit of Name * AttachementLevel * Exp
+               //|Value of Exp
+               |Return of Name * AttachementLevel
+
+let compile = Lexing.LexBuffer<char>.FromString >> FormulasParser.start FormulasLexer.tokenize
